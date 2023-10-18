3785e35fc0c6d841dc1c299f094b8b479cd8e12c
JaydenDaniello <jcd00013@mix.wvu.edu>
2023-10-18 18:29:11 -0400

    added requestToken and getProfile to getData

--- getData.js
+++ getData.js
@@ -25,9 +25,7 @@
 		}
 		
 		function requestAuthentication() {
-			const clientId = '7820cb5ed08b4ad490fcad0e33712d6e'; //client id is provided by spotify for webapps, but a redirect uri is required to get it
-			const redirectUri = 'http://127.0.0.1:5500/welcome.html';
-
+	
 			let codeVerifier = generateRandomString(128);
 
 			generateCodeChallenge(codeVerifier).then(codeChallenge => {
@@ -49,8 +47,26 @@
 				window.location = 'https://accounts.spotify.com/authorize?' + args;
 			});
             
+			requestToken();
 		}
 
+		async function getProfile() {
+			let accessToken = localStorage.getItem('access_token');
+		  
+			const response = await fetch('https://api.spotify.com/v1/me', {
+			  headers: {
+				Authorization: 'Bearer ' + accessToken
+			  }
+			});
+		  
+			const data = await response.json();
+			console.log(data);
+		  }
+
+		const clientId = '7820cb5ed08b4ad490fcad0e33712d6e'; //client id is provided by spotify for webapps, but a redirect uri is required to get it
+		const redirectUri = 'http://127.0.0.1:5500/welcome.html';
+
+		function requestToken() {
 		//parse URL and save code parameter to request access token
 		const urlParams = new URLSearchParams(window.location.search);
 		let code = urlParams.get('code');
@@ -64,4 +80,29 @@
 		client_id: clientId,
 		code_verifier: codeVerifier
 		});
+
+		const response = fetch('https://accounts.spotify.com/api/token', {
+  		method: 'POST',
+  		headers: {
+    		'Content-Type': 'application/x-www-form-urlencoded'
+  		},
+  		body: body
+	})
+ 	 .then(response => {
+    	if (!response.ok) {
+    	  throw new Error('HTTP status ' + response.status);
+    	}
+    	return response.json();
+  	})
+  	.then(data => {
+    	localStorage.setItem('access_token', data.access_token);
+  	})
+  	.catch(error => {
+    	console.error('Error:', error);
+  	});
+}
 		
+if (localStorage.getItem('access_token') != null) {
+	requestToken();
+	getProfile();
+}
