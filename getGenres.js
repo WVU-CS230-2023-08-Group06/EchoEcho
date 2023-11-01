6aac71824554a8983e960ef409b427af95543be2
CarsonV369 <143527878+CarsonV369@users.noreply.github.com>
2023-11-01 02:43:11 -0400

    Update getGenres.js to contain array flattening, chunking, genre compilation, and artist/song reference data

--- getGenres.js
+++ getGenres.js
@@ -1,3 +1,6 @@
+//function rturns artists based on saved artists on the potify account
+//recursively calls itself after an artist is received using spotify's callback url
+//returned as a promise
 function getArtistsFromSavedTracks(url, setOfArtists) {
   return fetch(url,
     { headers: {'Authorization': 'Bearer ' + token }}
@@ -18,28 +21,78 @@ function getArtistsFromSavedTracks(url, setOfArtists) {
     }
   });
 }
-
+//After artists are collected from getArtistsFromSavedTracks this function will take some and compares
+//how often genres among them are referenced returning these values into a list
 function getArtistGenreFromArtists(artistBatch) {
   return fetch("https://api.spotify.com/v1/artists/?ids=" + artistBatch,
     { headers: {'Authorization': 'Bearer ' + token }}
   ).then(function(data) {
     return data.json();
   }).then(function(data) {
-    let genreDict = {};
+    let genreList = {};
     data.artists.forEach(artist => {
-      genreDict = addToGenreDict(artist.genres, genreDict);
+      genreList = addToGenreList(artist.genres, genreList);
     })
-    return genreDict;
+    return genreList;
   });
 }
-
-function addToGenreDict(genre, genreDict) {
+//this function allows for the adding of those referenced genres to a list where they will get a value that corresponds
+//to how often they show up
+function addToGenreList(genre, genreList) {
   genre.forEach(genre => {
-    if (genreDict['genre']) {
-  genreDict['genre'] += 1;
+    if (genreList['genre']) {
+  genreList['genre'] += 1;
 } else {
-  genreDict['genre'] = 1;
+  genreList['genre'] = 1;
 }
   });
-  return genreDict;
+  return genreList;
 }
+fetch("https://api.spotify.com/v1/me",
+    { headers: {'Authorization': 'Bearer ' + token }}
+  ).then(function(user) {
+  //the returne her is a promise that will return a set
+    let setOfArtists = new Set();
+    return getArtistsFromSavedTracks(
+      'https://api.spotify.com/v1/me/tracks',
+      setOfArtists
+    );
+  }).then(function(setOfArtists) {
+    let genresPromises = [...setOfArtists]
+    .chunk(50)
+    .map(artistBatch => getArtistGenreFromArtists(artistBatch));
+  //compiling our subset into a batch of sets
+    return Promise.all(genresPromises);
+  }).then(function(genreList) {
+    var flattenedGenreList = genreList.reduce((result, currentObject) => {
+      for(var key in currentObject) {
+          if (currentObject.hasOwnProperty(key)) {
+            result[key] = (result[key] || 0) + currentObject[key];
+          }
+      }
+      return result;
+    }, {});
+    return flattenedGenreList;
+  });
+});
+//this function is applied to an array and then applies a chunk size of 50 to it, this way we don't have to convert the array
+Object.defineProperty(Array.prototype, 'chunk', {
+    value: function(chunkSize) {
+        var that = this;
+        return Array(Math.ceil(that.length/chunkSize)).fill().map(function(_,i){
+            return that.slice(i*chunkSize,i*chunkSize+chunkSize);
+        });
+    }
+});
+//last we will flatten the array by reducing it, iterating through the genres while cutting repeats, when a repeat is cut the value of that genre is incremented
+then(function(genreList) {
+  var flattenedGenreList = genreList.reduce((result, currentObject) => {
+    for(var key in currentObject) {
+        if (currentObject.hasOwnProperty(key)) {
+          result[key] = (result[key] || 0) + currentObject[key];
+        }
+    }
+    return result;
+  }, {});
+  return flattenedGenreList;
+});
