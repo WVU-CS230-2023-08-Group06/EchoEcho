8f6c7b398dbf23415d2f1bdbc0c343887cf0d817
JaydenDaniello <jcd00013@mix.wvu.edu>
2023-11-01 21:20:52 -0400

    Updated top artists page with links & pictures

--- displayArtists.js
+++ displayArtists.js
@@ -14,7 +14,19 @@ if (topArtistsString !== null && typeof topArtistsString === "string") {
 	if (topArtists) {
 		topArtists.forEach(function(artist) {
 			var listItem = document.createElement('li');
-			listItem.textContent = artist.name;
+			listItem.className = 'artistList';
+			
+			var artistPicture = document.createElement('img');
+			artistPicture.src = artist.images[0].url;
+			artistPicture.className = 'artistImg';
+
+			var artistLink = document.createElement('a');
+			artistLink.href = artist.external_urls.spotify;
+			artistLink.textContent = artist.name;
+			artistLink.className = 'artistLinks';
+
+			listItem.appendChild(artistPicture);
+			listItem.appendChild(artistLink);
 			console.log(artist.name);
 			artistList.appendChild(listItem);
 		});
