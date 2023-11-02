e892f258db0f31c2a77f770da86c8480dc61d700
alexwalkerr <122568397+alexwalkerr@users.noreply.github.com>
2023-11-02 00:25:08 -0400

    Add files via upload
    Added topTracks page and javascript

--- getData.js
+++ getData.js
@@ -164,4 +164,41 @@ if (localStorage.getItem('access_token') != null) {
 	getTopArtists();
 }
 
-
+//Function to get top tracks
+// async function getTopTracks() {
+// 	let accessToken = localStorage.getItem('access_token');
+  
+// 	// Initialize an empty array to store all top artists
+// 	let allTracks = [];
+  
+// 	async function fetchTopTracks(offset = 0) {
+// 	  const response = await fetch(`https://api.spotify.com/v1/me/top/tracks?limit=20&offset=${offset}`, {
+// 		headers: {
+// 		  Authorization: 'Bearer ' + accessToken,
+// 		},
+// 	  });
+  
+// 	  const data = await response.json();
+// 	  if (data.items && data.items.length > 0) {
+// 		allTracks = allTracks.concat(data.items);
+// 		// Check for pagination and fetch the next page if available
+// 		if (data.next) {
+// 		  const nextOffset = new URL(data.next).searchParams.get('offset');
+// 		  await fetchTopTracks(nextOffset);
+// 		} else {
+// 			localStorage.setItem('top_tracks', JSON.stringify(allTracks));
+// 			let topdbug = localStorage.getItem('top_tracks')
+// 		  	console.log(allTracks); // All top tracks retrieved
+			
+// 		}
+// 	  }
+// 	}
+  
+// 	await fetchTopTracks();
+//   }
+		
+// if (localStorage.getItem('access_token') != null) {
+// 	requestToken();
+// 	getProfile();
+// 	getTopTracks();
+// }
