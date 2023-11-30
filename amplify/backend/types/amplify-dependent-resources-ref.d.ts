2b535174852e6d1f38e0ddce773ed17922c659fd
sdFrasch <frasch.sam@gmail.com>
2023-11-30 14:22:32 -0500

    test 1

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "echoecho": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
