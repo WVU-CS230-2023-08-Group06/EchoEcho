01135271fbb2e630a4e031a41c8275492d4ce073
JaydenDaniello <jcd00013@mix.wvu.edu>
2023-12-07 11:58:14 -0500

    changed uri for debugging

--- getData.js
+++ getData.js
@@ -60,7 +60,7 @@ function requestAuthentication() {
 }
 
 const clientId = '850b3d0b9dba47c689f25160b90f1448'; //client id is provided by spotify for webapps, but a redirect uri is required to get it
-const redirectUri = 'https://main.d3ontvtqcgyr6j.amplifyapp.com/homepage.html';
+const redirectUri = 'https://main.d3ontvtqcgyr6j.amplifyapp.com/';
 
 function requestToken() {
 //parse URL and save code parameter to request access token
