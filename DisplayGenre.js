b1c0d219a37793dc5b4b8fd45cf70bee3861d68a
CarsonV369 <143527878+CarsonV369@users.noreply.github.com>
2023-12-11 02:16:08 -0500

    Update and rename genre.js to DisplayGenre.js

---
+++ DisplayGenre.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+function logout() {
+	localStorage.clear();
+	window.location.href = "https://main.d3ontvtqcgyr6j.amplifyapp.com";
+}
+function displayGenres() {
+    var topGenresString = getGenres();
+    var topGenres = JSON.parse(topGenresString);
+    console.log("successfully retreived 'tasks' and contents.");
+
+    if(getGenre != null){
+        topGenres.forEach(function(genre){
+            var listItem = document.createElement('li');
+            listItem.className = 'genreList';
+
+            console.log(track.name);
+			trackList.appendChild(listItem);
+        })
+    }
+
+    document.addEventListener('DOMContentLoaded', function () {
+        displayGenres();
+    });
+
+}
