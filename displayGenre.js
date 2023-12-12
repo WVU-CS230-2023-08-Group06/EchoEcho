100605bb45e1cf76cf572aae03b0b55e269554cb
CarsonV369 <143527878+CarsonV369@users.noreply.github.com>
2023-12-12 00:33:22 -0500

    Update displayGenre.js

--- displayGenre.js
+++ displayGenre.js
@@ -1,35 +1,22 @@
-// JavaScript Document
-function logout() {
-	localStorage.clear();
-	window.location.href = "https://main.d3ontvtqcgyr6j.amplifyapp.com";
-}
 function displayGenre() {
-	const authToken = localStorage.getItem('auth_token');
-	//get the array of top artists
-	var topArtistsString = localStorage.getItem('top_artists');
-	//Ensure that the array exists
-	if (topArtistsString !== null && typeof topArtistsString === "string") {
-		console.log(topArtistsString); //for debugging
-		var topArtists = JSON.parse(topArtistsString);   // deserializing here
-		console.log("Succesfully retrieved 'tasks' and contents.");
-	}
-
-    
-	//get the list element from topArtistsPage
-	var genreList = document.getElementById('genre');
-	if (topArtists) {
-		topArtists.forEach(function(artist) { //for each JSON object in the array of artists
-			//Create the list element for the genre
-			var listItem = document.createElement('li');
-			listItem.className = 'genreList';
-			const trackName = document.createElement('span');
-            genreName.textContent = genre.name;
-            genreName.className = 'genreName';
-			console.log(artist.genre)
-			genreList.appendChild(listItem);
-        })
-    }
-	document.addEventListener('DOMContentLoaded', function () {
-        displayGenres();
+    // Sort genreArray based on the count (genreArray[i][1]) in descending order
+    genreArray.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+    var genreListElement = document.getElementById('genreList');
+    // Clear the existing content in the genreListElement
+    genreListElement.innerHTML = '';
+    // Iterate through the sorted genreArray and display each genre
+    genreArray.forEach(function(genre) {
+        var listItem = document.createElement('li');
+        listItem.textContent = genre[0] + ' - ' + genre[1] + ' occurrences';
+        genreListElement.appendChild(listItem);
     });
 }
+// Call displayGenre after calling getGenres
+// You should call displayGenre after the data is ready to be displayed
+// Assuming you have an HTML button with id "displayButton" to trigger the display
+document.addEventListener('DOMContentLoaded', function () {
+	getGenres();
+	displayTracks();
+});
