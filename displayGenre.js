78df783f84aa19c5e22a1efd7934dfc077cdd53c
CarsonV369 <cdv0010@mix.wvu.edu>
2023-12-13 14:59:51 -0500

    changes to displayGenre

--- displayGenre.js
+++ displayGenre.js
@@ -1,8 +1,8 @@
 // Retrieve genreArray from local storage
 var genreArray = JSON.parse(localStorage.getItem('genre_array')) || [];
 
-// Function to display the genres
-function displayGenres() {
+// Function to display the top 20 genres
+function displayTopGenres() {
     // Sort genreArray based on the count (genreArray[i][1]) in descending order
     genreArray.sort(function(a, b) {
         return b[1] - a[1];
@@ -14,13 +14,13 @@ function displayGenres() {
     // Clear the existing content in the genreListElement
     genreListElement.innerHTML = '';
 
-    // Display all genres
-    for (var i = 0; i < genreArray.length; i++) {
+    // Display only the top 20 genres
+    for (var i = 0; i < Math.min(20, genreArray.length); i++) {
         var listItem = document.createElement('li');
         listItem.textContent = genreArray[i][0] + ' - ' + genreArray[i][1] + ' occurrences';
         genreListElement.appendChild(listItem);
     }
 }
 
-// Trigger displayGenres when the window is loaded
-window.addEventListener('load', displayGenres);
+// Trigger displayTopGenres when the window is loaded
+window.addEventListener('load', displayTopGenres);
